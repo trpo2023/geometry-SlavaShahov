a6c6876bb8a70b8ffb139b44f717c5e80b4da048
SlavaShahov <root@DESKTOP-SFEJ0QL>
2023-02-25 21:37:34 +0700

    Add Makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,4 @@
 all: main
 main: main.c
 	gcc -Wall -Werror -o main main.c
-run:
 	./main
-	
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	printf("GitHub");
 
+	printf("GitHub\n");
 	return 0;
 }
